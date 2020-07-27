ad177aee29f657237e2f92ad4f278b6295879a24
djmcp <pete.j.mcdowell@gmail.com>
2020-07-27 11:31:07 -0500

    Update update_ci_version.sh
    Change to use a temp file for the gitlab ci

--- update_ci_version.sh
+++ update_ci_version.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd /srv/docker/hass
 HA_VER=$(<.HA_VERSION)
-cat .gitlab-ci.yml | /snap/bin/yq w - variables.HA_VERSION $HA_VER | tee .gitlab-ci.yml
+cat .gitlab-ci.yml | /snap/bin/yq w - variables.HA_VERSION $HA_VER | tee temp_gitlab-ci.yml
+rm prev_gitlab-ci.yml
+mv .gitlab-ci.yml prev_gitlab-ci.yml
+mv temp_gitlab-ci.yml .gitlab-ci.yml
 git add .gitlab-ci.yml
 git commit -m "Update CI current HA Version to $HA_VER [ci skip]"
 git push
