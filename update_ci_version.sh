4bf1b80aa029d3758da23b70f40df4bb81f125d7
djmcp <pete.j.mcdowell@gmail.com>
2020-07-26 22:19:34 -0500

    Update update_ci_version.sh

--- update_ci_version.sh
+++ update_ci_version.sh
@@ -2,6 +2,6 @@
 cd /srv/docker/hass
 HA_VER=$(<.HA_VERSION)
 cat .gitlab-ci.yml | yq w - variables.HA_VERSION $HA_VER | tee .gitlab-ci.yml
-# git add .gitlab-ci.yml
-# git commit -m "Update HA Version to $HA_VER [ci skip]"
-# git push
+git add .gitlab-ci.yml
+git commit -m "Update HA Version to $HA_VER [ci skip]"
+git push
